64fc44678d5860b80243d989580e161251fbbc5c
Matteo Fregiari <matteo.fregiari@vidiemme.it>
2018-09-27 15:12:33 +0200

    :tada: wip: Commit iniziale

---
+++ spyfall/lib/locations3.js
@@ -0,0 +1,35 @@
+locations3 = [
+	{
+		name: 'locations.night club',
+		roles: [
+			'locations.roles.night club.stripper',
+			'locations.roles.night club.cubist',
+            'locations.roles.night club.horny man',
+            'locations.roles.night club.drunkard',
+            'locations.roles.night club.owner',
+            'locations.roles.night club.cheating man',
+            'locations.roles.night club.married man',
+            'locations.roles.night club.cubist'
+		]
+	},
+	{
+		name: 'locations.red light cinema',
+		roles: [
+			'locations.roles.red light cinema.ticket seller',
+			'locations.roles.red light cinema.porn actress',
+			'locations.roles.red light cinema.student',
+			'locations.roles.red light cinema.horny teacher',
+			'locations.roles.red light cinema.wurstel seller',
+			'locations.roles.red light cinema.maniac',
+			'locations.roles.red light cinema.married man',
+			'locations.roles.red light cinema.old man'
+		]
+	},
+    {
+        name: 'locations.trans dating house',
+        roles: [
+            'locations.roles.trans dating house.little trans',
+            'locations.roles.red light cinema.politician'
+        ]
+    }
+];
