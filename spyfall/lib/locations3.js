19e1301f4d03fe3df07b04ae5ddf0d6017fb80f9
Matteo Fregiari <matteo.fregiari@vidiemme.it>
2018-09-28 16:40:30 +0200

    :construction: wip: Aggiunto studio di leonardo alle location3

--- spyfall/lib/locations3.js
+++ spyfall/lib/locations3.js
@@ -3,14 +3,13 @@ locations3 = [
 		name: 'locations.leonard studio',
         translation: 'Lo studio di Leonardo da Vinci',
 		roles: [
-			'locations.roles.night club.stripper',
-			'locations.roles.night club.cubist',
-            'locations.roles.night club.horny man',
-            'locations.roles.night club.drunkard',
-            'locations.roles.night club.owner',
-            'locations.roles.night club.cheating man',
-            'locations.roles.night club.married man',
-            'locations.roles.night club.cubist'
+			'locations.roles.leonard studio.leonardo',
+			'locations.roles.leonard studio.verrocchio',
+            'locations.roles.leonard studio.lorenzo',
+            'locations.roles.leonard studio.macchiavelli',
+            'locations.roles.leonard studio.borgia',
+            'locations.roles.leonard studio.michelangelo',
+            'locations.roles.leonard studio.ludovico'
 		]
 	}
 ];
