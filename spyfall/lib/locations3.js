b1d75502e30b879be2977f7b174a6bc05bdbefa7
Matteo Fregiari <matteo.fregiari@vidiemme.it>
2018-10-01 18:31:29 +0200

    :construction: wip: Aggiunti luoghi ambientazione 3

--- spyfall/lib/locations3.js
+++ spyfall/lib/locations3.js
@@ -16,13 +16,234 @@ locations3 = [
         name: 'locations.pyramid',
         translation: 'Cantiere di costruzione delle piramidi',
         roles: [
-            'locations.roles.pyramid.faraone',
-            'locations.roles.pyramid.schiavo',
-            'locations.roles.pyramid.frustatore',
-            'locations.roles.pyramid.guardia',
-            'locations.roles.pyramid.capo cantiere',
-            'locations.roles.pyramid.operaio',
-            'locations.roles.pyramid.sacerdote'
+            'locations.roles.pyramid.player1',
+            'locations.roles.pyramid.player2',
+            'locations.roles.pyramid.player3',
+            'locations.roles.pyramid.player4',
+            'locations.roles.pyramid.player5',
+            'locations.roles.pyramid.player6',
+            'locations.roles.pyramid.player7'
+        ]
+    },
+    {
+        name: 'locations.hot air balloon',
+        translation: 'Mongolfiera',
+        roles: [
+            'locations.roles.hot air balloon.player1',
+            'locations.roles.hot air balloon.player2',
+            'locations.roles.hot air balloon.player3',
+            'locations.roles.hot air balloon.player4',
+            'locations.roles.hot air balloon.player5',
+            'locations.roles.hot air balloon.player6',
+            'locations.roles.hot air balloon.player7'
+        ]
+    },
+    {
+        name: 'locations.spanish inquisition',
+        translation: 'Inquisizione spagnola',
+        roles: [
+            'locations.roles.spanish inquisition.player1',
+            'locations.roles.spanish inquisition.player2',
+            'locations.roles.spanish inquisition.player3',
+            'locations.roles.spanish inquisition.player4',
+            'locations.roles.spanish inquisition.player5',
+            'locations.roles.spanish inquisition.player6',
+            'locations.roles.spanish inquisition.player7'
+        ]
+    },
+    {
+        name: 'locations.caravan',
+        translation: 'Carovana',
+        roles: [
+            'locations.roles.caravan.player1',
+            'locations.roles.caravan.player2',
+            'locations.roles.caravan.player3',
+            'locations.roles.caravan.player4',
+            'locations.roles.caravan.player5',
+            'locations.roles.caravan.player6',
+            'locations.roles.caravan.player7'
+        ]
+    },
+    {
+        name: 'locations.druid circle',
+        translation: 'Circolo di druidi',
+        roles: [
+            'locations.roles.druid circle.player1',
+            'locations.roles.druid circle.player2',
+            'locations.roles.druid circle.player3',
+            'locations.roles.druid circle.player4',
+            'locations.roles.druid circle.player5',
+            'locations.roles.druid circle.player6',
+            'locations.roles.druid circle.player7'
+        ]
+    },
+    {
+        name: 'locations.lunar base',
+        translation: 'Base lunare',
+        roles: [
+            'locations.roles.lunar base.player1',
+            'locations.roles.lunar base.player2',
+            'locations.roles.lunar base.player3',
+            'locations.roles.lunar base.player4',
+            'locations.roles.lunar base.player5',
+            'locations.roles.lunar base.player6',
+            'locations.roles.lunar base.player7'
+        ]
+    },
+    {
+        name: 'locations.shaolin temple',
+        translation: 'Tempio Shaolin',
+        roles: [
+            'locations.roles.shaolin temple.player1',
+            'locations.roles.shaolin temple.player2',
+            'locations.roles.shaolin temple.player3',
+            'locations.roles.shaolin temple.player4',
+            'locations.roles.shaolin temple.player5',
+            'locations.roles.shaolin temple.player6',
+            'locations.roles.shaolin temple.player7'
+        ]
+    },
+    {
+        name: 'locations.speakeasy',
+        translation: 'Proibizionismo',
+        roles: [
+            'locations.roles.speakeasy.player1',
+            'locations.roles.speakeasy.player2',
+            'locations.roles.speakeasy.player3',
+            'locations.roles.speakeasy.player4',
+            'locations.roles.speakeasy.player5',
+            'locations.roles.speakeasy.player6',
+            'locations.roles.speakeasy.player7'
+        ]
+    },
+    {
+        name: 'locations.musketeers',
+        translation: 'Compagnia di moschettieri',
+        roles: [
+            'locations.roles.musketeers.player1',
+            'locations.roles.musketeers.player2',
+            'locations.roles.musketeers.player3',
+            'locations.roles.musketeers.player4',
+            'locations.roles.musketeers.player5',
+            'locations.roles.musketeers.player6',
+            'locations.roles.musketeers.player7'
+        ]
+    },
+    {
+        name: 'locations.medieval tournament',
+        translation: 'Torneo medievale',
+        roles: [
+            'locations.roles.medieval tournament.player1',
+            'locations.roles.medieval tournament.player2',
+            'locations.roles.medieval tournament.player3',
+            'locations.roles.medieval tournament.player4',
+            'locations.roles.medieval tournament.player5',
+            'locations.roles.medieval tournament.player6',
+            'locations.roles.medieval tournament.player7'
+        ]
+    },
+    {
+        name: 'locations.trojan war',
+        translation: 'Guerra di Troia',
+        roles: [
+            'locations.roles.trojan war.player1',
+            'locations.roles.trojan war.player2',
+            'locations.roles.trojan war.player3',
+            'locations.roles.trojan war.player4',
+            'locations.roles.trojan war.player5',
+            'locations.roles.trojan war.player6',
+            'locations.roles.trojan war.player7'
+        ]
+    },
+    {
+        name: 'locations.woodstock',
+        translation: 'Festival di Woodstock',
+        roles: [
+            'locations.roles.woodstock.player1',
+            'locations.roles.woodstock.player2',
+            'locations.roles.woodstock.player3',
+            'locations.roles.woodstock.player4',
+            'locations.roles.woodstock.player5',
+            'locations.roles.woodstock.player6',
+            'locations.roles.woodstock.player7'
+        ]
+    },
+    {
+        name: 'locations.harem',
+        translation: 'Harem imperiale ottomano',
+        roles: [
+            'locations.roles.harem.player1',
+            'locations.roles.harem.player2',
+            'locations.roles.harem.player3',
+            'locations.roles.harem.player4',
+            'locations.roles.harem.player5',
+            'locations.roles.harem.player6',
+            'locations.roles.harem.player7'
+        ]
+    },
+    {
+        name: 'locations.world war airship',
+        translation: 'Dirigibile della prima guerra mondiale',
+        roles: [
+            'locations.roles.world war airship.player1',
+            'locations.roles.world war airship.player2',
+            'locations.roles.world war airship.player3',
+            'locations.roles.world war airship.player4',
+            'locations.roles.world war airship.player5',
+            'locations.roles.world war airship.player6',
+            'locations.roles.world war airship.player7'
+        ]
+    },
+    {
+        name: 'locations.ancient olympic',
+        translation: 'Giochi olimpici antichi',
+        roles: [
+            'locations.roles.ancient olympic.player1',
+            'locations.roles.ancient olympic.player2',
+            'locations.roles.ancient olympic.player3',
+            'locations.roles.ancient olympic.player4',
+            'locations.roles.ancient olympic.player5',
+            'locations.roles.ancient olympic.player6',
+            'locations.roles.ancient olympic.player7'
+        ]
+    },
+    {
+        name: 'locations.roman senate',
+        translation: 'Senato romano',
+        roles: [
+            'locations.roles.roman senate.player1',
+            'locations.roles.roman senate.player2',
+            'locations.roles.roman senate.player3',
+            'locations.roles.roman senate.player4',
+            'locations.roles.roman senate.player5',
+            'locations.roles.roman senate.player6',
+            'locations.roles.roman senate.player7'
+        ]
+    },
+    {
+        name: 'locations.gold rush mining camp',
+        translation: 'Campo minerario della corsa all\'oro',
+        roles: [
+            'locations.roles.gold rush mining camp.player1',
+            'locations.roles.gold rush mining camp.player2',
+            'locations.roles.gold rush mining camp.player3',
+            'locations.roles.gold rush mining camp.player4',
+            'locations.roles.gold rush mining camp.player5',
+            'locations.roles.gold rush mining camp.player6',
+            'locations.roles.gold rush mining camp.player7'
+        ]
+    },
+    {
+        name: 'locations.viking longship',
+        translation: 'Imbarcazione vichinga',
+        roles: [
+            'locations.roles.viking longship.player1',
+            'locations.roles.viking longship.player2',
+            'locations.roles.viking longship.player3',
+            'locations.roles.viking longship.player4',
+            'locations.roles.viking longship.player5',
+            'locations.roles.viking longship.player6',
+            'locations.roles.viking longship.player7'
         ]
     }
 ];
